c90b508a2ad3716f42fa1c467eeffdabc4fc048c
Dainius Armonas <armonas@yahoo.com>
2016-09-12 13:49:11 +0300

    Stiliams naudoti scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.sass']
 })
 export class AppComponent {
   title = 'app works!';
